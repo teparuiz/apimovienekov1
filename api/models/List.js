d670271c79d181722b207945496feba0e5c699b6
Rodrigo Ruiz <rodrigo@MacBook-Air-de-Rodrigo.local>
2023-05-24 23:28:13 -0600

    first commit

---
+++ api/models/List.js
@@ -0,0 +1,18 @@
+module.exports = {
+  attributes: {
+    id: {
+      type: "string",
+      required: true,
+    },
+
+    description: {
+      type: "string",
+      required: true,
+    },
+
+    calification: {
+      type: "string",
+      required: true,
+    },
+  },
+};
